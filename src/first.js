83d92bcf0b570fef376c334faf89d82049d56ecd
Dhruv Srivastava <dhruvdhs.ds@gmail.com>
2020-05-19 02:23:07 +0530

    adding /src

---
+++ src/first.js
@@ -0,0 +1,14 @@
+console.log("HEllo  world");
+
+var a = 10;
+var i;
+function heya(){
+	for (let i = 0; i<a;i++){
+
+		console.log(i);
+	}
+}
+
+for(let l =0;l<5;l++){
+	heya();
+}
---
+++ src/second.py
@@ -0,0 +1,6 @@
+from math import *
+
+boy = [1,2,3,4,5,6,7,8,9]
+aye = ["a","b","c"]
+aye.extend(boy[1:5])
+print(aye)
